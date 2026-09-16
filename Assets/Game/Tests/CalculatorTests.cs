a5c21d4fcd4431007da0e3839588b48098b55b76
Vyacheslav <snego.bro@gmail.com>
2026-09-16 23:50:42 +0700

    feat: tests

--- Assets/Game/Tests/CalculatorTests.cs
+++ Assets/Game/Tests/CalculatorTests.cs
@@ -7,12 +7,72 @@ namespace Tests
     public class CalculatorTests
     {
         [Test]
-        public void FindSumBetweenFirstAndLastNegativeElement_Normal()
+        public void FindSumBetweenFirstAndLastNegativeElement_EmptyList()
         {
             // Arrange.
             Calculator calculator = new();
-            List<int> numbers = new() { -1, 1, 1, 1, -1 };
-            int expectedResult = 3;
+            List<int> numbers = new() { };
+            int expectedResult = -1;
+
+            // Act.
+            int actualResult = calculator.FindSumBetweenFirstAndLastNegativeElement(numbers);
+
+            // Assert.
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void FindSumBetweenFirstAndLastNegativeElement_NegativeNumberFoundImmediately()
+        {
+            // Arrange.
+            Calculator calculator = new();
+            List<int> numbers = new() { -1 };
+            int expectedResult = 0;
+
+            // Act.
+            int actualResult = calculator.FindSumBetweenFirstAndLastNegativeElement(numbers);
+
+            // Assert.
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void FindSumBetweenFirstAndLastNegativeElement_NoNegativeElements()
+        {
+            // Arrange.
+            Calculator calculator = new();
+            List<int> numbers = new() { 1 };
+            int expectedResult = -1;
+
+            // Act.
+            int actualResult = calculator.FindSumBetweenFirstAndLastNegativeElement(numbers);
+
+            // Assert.
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void FindSumBetweenFirstAndLastNegativeElement_SecondLoopSkipsNonNegativeNumber()
+        {
+            // Arrange.
+            Calculator calculator = new();
+            List<int> numbers = new() { -1, 1 };
+            int expectedResult = 0;
+
+            // Act.
+            int actualResult = calculator.FindSumBetweenFirstAndLastNegativeElement(numbers);
+
+            // Assert.
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void FindSumBetweenFirstAndLastNegativeElement_SumElementsBetweenNegative()
+        {
+            // Arrange.
+            Calculator calculator = new();
+            List<int> numbers = new() { -1, 1, 1, -1 };
+            int expectedResult = 2;
 
             // Act.
             int actualResult = calculator.FindSumBetweenFirstAndLastNegativeElement(numbers);
